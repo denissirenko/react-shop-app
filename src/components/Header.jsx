ac3c937b5bcd6d53878469df463ce908b823733a
Denis Sirenko <sirenko.denis86@gmail.com>
2021-11-24 21:07:48 +0200

    init proj

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+export const Header = () => {
+    return <nav className="green darken-1">
+        <div className="nav-wrapper">
+        <a href="https://github.com/denissirenko" className="brand-logo">React Shop</a>
+        <ul id="nav-mobile" className="right hide-on-med-and-down">
+            <li><a href="https://github.com/denissirenko">My Repo</a></li>
+        </ul>
+        </div>
+    </nav>
+}
