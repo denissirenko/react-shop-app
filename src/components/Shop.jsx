f73a3f0e5b31c7529f0bbc2b2c1af5dcec4b4282
Denis Sirenko <sirenko.denis86@gmail.com>
2021-11-25 20:31:13 +0200

    add basket

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -3,10 +3,86 @@ import { API_KEY, API_URL } from '../config';
 
 import { Preloader } from './Preloader';
 import { GoodsList } from './GoodsList';
+import { Card } from './Card';
+import { BasketList } from './BasketList';
+import { Alert } from './Alert';
 
 export const Shop = () => {
     const [goods, setGoods] = useState([]);
     const [loading, setLoading] = useState(true);
+    const [order, setOrder] = useState([]);
+    const [isBasketShow, setisBasketShow] = useState(false);
+    const [alertName, setAlertName] = useState('');
+
+    const addToBasket = (item) => {
+        const itemIndex = order.findIndex(orderItem => orderItem.id === item.id);
+
+        if (itemIndex < 0) {
+            const newItem = {
+                ...item,
+                quantity: 1
+            }
+            setOrder(
+                [...order, newItem]
+            ); 
+        } else {
+            const newOrder = order.map((orderItem, index) => {
+                if(index === itemIndex) {
+                    return {
+                        ...orderItem,
+                        quantity: orderItem.quantity + 1
+                    }
+                } else {
+                    return orderItem;
+                }
+            });
+            setOrder(newOrder);
+        }
+        setAlertName(item.name);
+    };
+
+    const removeFromBasket = (itemId) => {
+        const newOrder = order.filter((el) => el.id !== itemId);
+        setOrder(newOrder);
+    };
+
+    const incQuantity = (itemId) => {
+        const newOrder = order.map((el) => {
+            if (el.id === itemId) {
+                const newQuantity = el.quantity + 1;
+                return {
+                    ...el,
+                    quantity: newQuantity,
+                };
+            } else {
+                return el;
+            }
+        });
+        setOrder(newOrder);
+    };
+    
+    const decQuantity = (itemId) => {
+        const newOrder = order.map((el) => {
+            if (el.id === itemId) {
+                const newQuantity = el.quantity - 1;
+                return {
+                    ...el,
+                    quantity: newQuantity >= 0 ? newQuantity : 0,
+                };
+            } else {
+                return el;
+            }
+        });
+        setOrder(newOrder);
+    };
+
+    const handleBasketShow = () => {
+        setisBasketShow(!isBasketShow);
+    };
+
+    const closeAlert = () => {
+        setAlertName('');
+    };
 
     useEffect( function getGoods() {
         fetch(API_URL, {
@@ -23,7 +99,18 @@ export const Shop = () => {
 
     return (
         <div className='container content'>
-            {loading ? <Preloader /> : <GoodsList goods={goods} />}
+            <Card quantity={order.length} handleBasketShow={handleBasketShow}/>
+            {loading ? <Preloader /> : <GoodsList goods={goods} addToBasket={addToBasket} />}
+            {isBasketShow && (
+                <BasketList
+                    order={order}
+                    handleBasketShow={handleBasketShow}
+                    removeFromBasket={removeFromBasket}
+                    incQuantity={incQuantity}
+                    decQuantity={decQuantity}
+                />
+            )}
+            {alertName && <Alert name={alertName} closeAlert={closeAlert} />}
         </div>
     )
 }
