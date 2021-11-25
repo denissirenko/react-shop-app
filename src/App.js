44bbfe59b1a128a1358474957d5be0b8469009b2
Denis Sirenko <sirenko.denis86@gmail.com>
2021-11-25 11:42:39 +0200

    add goods

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { Header } from './components/Header';
 import { Footer } from './components/Footer';
+import { Shop } from './components/Shop';
 
 function App() {
   return (
     <>
       <Header />
+      <Shop />
       <Footer />
     </>
   );
